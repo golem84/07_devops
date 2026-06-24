6fa897451d3a02e5302170dc3bdeea0c168e06e2
golem84 <andr0073@yandex.ru>
2026-06-24 23:28:40 +0800

    update app.py

--- flask_redis/app.py
+++ flask_redis/app.py
@@ -9,13 +9,13 @@ cache = redis.Redis(host='redis', port=6379)
 def get_hit_count():
     retires = 5
     while True:
-            try:
-                 return cache.incr('hits')
-            except redis.exceptions.ConnectionError as exc:
-                 if retries == 0:
+          try:
+               return cache.incr('hits')
+          except redis.exceptions.ConnectionError as exc:
+               if retries == 0:
                     raise exc
-                 retries -= 1
-                 time.sleep(0.5)
+               retries -= 1
+               time.sleep(0.5)
 
 @app.route('/')
 def hello():
