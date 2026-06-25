403e35535feada3db910af51b61deb8cda700ab3
golem84 <andr0073@yandex.ru>
2026-06-25 13:02:02 +0800

    update flask-app version

--- flask_redis/app.py
+++ flask_redis/app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times. My name is: {}\n'.format(count, socket.gethostname())
+    return 'Hello World, VER: 2! I have been seen {} times. My name is: {}\n'.format(count, socket.gethostname())
